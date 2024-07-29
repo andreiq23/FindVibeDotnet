f2223d489395c8318593910705158e844e56281e
Andi <andy.u789@gmail.com>
2024-07-29 12:45:02 +0300

    miaw

--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options => options.AddDefaultPolicy(builder => builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()));
 
 var app = builder.Build();
 
@@ -51,5 +52,6 @@ app.MapGet("/songs", async (string? search) =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.UseCors();
 app.Run();
 
